f3e53d8bf963d5a277d26d9a14abbf15f58dfd9a
Abishek Jayan <abishekjayan98@gmail.com>
2024-06-30 18:21:19 +0530

    Improved UI and updated prompt engineering

--- lib/camera_screen/camera_screen.dart
+++ lib/camera_screen/camera_screen.dart
@@ -15,6 +15,7 @@ class CameraScreen extends StatefulWidget {
   _CameraScreenState createState() => _CameraScreenState();
 }
 
+
 class _CameraScreenState extends State<CameraScreen> {
   CameraController? _controller;
   Future<void>? _initializeControllerFuture;
@@ -34,8 +35,9 @@ class _CameraScreenState extends State<CameraScreen> {
       firstCamera,
       ResolutionPreset.high,
     );
-
+    _controller!.setFlashMode(FlashMode.off);
     _initializeControllerFuture = _controller!.initialize();
+    
     setState(() {});
   }
 
@@ -45,46 +47,68 @@ class _CameraScreenState extends State<CameraScreen> {
     super.dispose();
   }
 
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Camera')),
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return CameraPreview(_controller!);
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.camera),
-        onPressed: () async {
-          try {
-            await _initializeControllerFuture;
-            final image = await _controller!.takePicture();
-            imageData = await image.readAsBytes();
-            setState(() {});
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(content: Text('Picture taken')),
-            );
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => DisplayPictureScreen(imageData: imageData!),
+      body: Stack(
+        children: [
+          FutureBuilder<void>(
+            future: _initializeControllerFuture,
+            builder: (context, snapshot) {
+              
+              if (snapshot.connectionState == ConnectionState.done) {
+                return SizedBox.expand(child: CameraPreview(_controller!));
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+          Positioned(
+            bottom: 50.0,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: FloatingActionButton.large(
+                child: Icon(Icons.camera),
+                onPressed: () async {
+                  try {
+                    await _initializeControllerFuture;
+                    _controller!.setFlashMode(FlashMode.off);
+                    final image = await _controller!.takePicture();
+                    _controller!.initialize();
+                    
+                    imageData = await image.readAsBytes();
+
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Picture taken')),
+                    );
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DisplayPictureScreen(imageData: imageData!),
+                      ),
+                    );
+                  } catch (e) {
+                    print(e);
+                  }
+                },
               ),
-            );
-          } catch (e) {
-            print(e);
-          }
-        },
+            ),
+          ),
+        ],
       ),
     );
   }
 }
 
+
+
+
+
+
+//Next Page
 class DisplayPictureScreen extends StatefulWidget {
   final Uint8List imageData;
 
@@ -110,13 +134,13 @@ class _DisplayPictureScreenState extends State<DisplayPictureScreen> {
     final prompt = TextPart('''
 Fill in the following details about this plant.
 Give those details in proper JSON format with no extra characters. This is mandatory.
-Give me only numbers, percentages, or 1-word answers.
-If you are not sure on any value, just write NA. If your are not sure of the plant's name, just make up a name.
-    name:
-    scientific_name:
-    sunlight:
-    temp:
-    water:
+ 
+    name: Give me the plant's commonly known name here. If you are not sure of the value just write NA.
+    scientific_name: Give me the plant's scientific name here. If you are not sure of the value just write NA.
+    sunlight: A percentage value with unit for the optimal amount of sunlight the plant needs. If you are not sure of the value just give me an average optimal percentage of sunlight; plants of this family need. You may use ranges if not sure. The unit should be in hours/day
+    temp: A Celsius value with unit for the optimal environmental temperature for the plant. If you are not sure of the value just give me an average optimal environmental temperature plants of this family need. You may use ranges if not sure. The unit should be in °C.
+    water: A litres value with unit for the optimal amount of water the plant needs. If you are not sure of the value just give me an average optimal litres value of water; plants of this family need. You may use ranges if not sure. The unit should be in l/day.
+    about: A short 25 word description of the plant. If you are not sure of the value just write No Description.
     ''');
     final imagePart = DataPart('image/jpeg', imageData);
 
@@ -133,83 +157,150 @@ If you are not sure on any value, just write NA. If your are not sure of the pla
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
-    final imageHeight = screenSize.height / 2;
-
-    return Scaffold(
-  appBar: AppBar(title: Text("Plant Stats")),
-  body: Center(
-
-    child: plantDetails == null ? CircularProgressIndicator() : Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Text(
-                      "Name: ${plantDetails!['name']}",
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      "Scientific Name: ${plantDetails!['scientific_name']}",
-                      style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
-                    ),
-                  ],
-                ),
-              ),
-              GestureDetector(
+  GestureDetector imageDisplayCreation(Image image, final croppedHeight) {
+    return GestureDetector(
                 onTap: () {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
                       builder: (context) => Scaffold(
-                        appBar: AppBar(),
-                        body: Center(
+                        body: SizedBox.expand(
                           child: Image.memory(imageData),
                         ),
                       ),
                     ),
                   );
                 },
-                child: Container(
-                  height: imageHeight,
-                  width: double.infinity,
-                  child: Image.memory(imageData, fit: BoxFit.cover),
+                                child: Container(
+                  height: croppedHeight,
+                  width: double.infinity, // Full width
+                  margin: EdgeInsets.symmetric(horizontal: 11.0), // 11.0 space on left and right
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: Image.memory(
+                      widget.imageData,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+              ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final screenSize = MediaQuery.of(context).size;
+        final croppedHeight = screenSize.height / 2;
+        return Scaffold(
+          appBar: AppBar(title: Text("Plant Details"), backgroundColor: Color.fromARGB(117, 44, 120, 101),),
+          body: 
+        
+        plantDetails == null ? Container(decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Color.fromARGB(117, 44, 120, 101), Color.fromARGB(223, 217, 237, 191)],
+                ),
+              ),child: Center(child: CircularProgressIndicator())) : 
+        
+        plantDetails!['name'] == "NA" ? Center(child: Container(decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Color.fromARGB(117, 44, 120, 101), Color.fromARGB(223, 217, 237, 191)],
                 ),
               ),
-              Expanded(
-                child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      _infoBox("Sunlight", plantDetails!['sunlight']),
-                      _infoBox("Temperature", plantDetails!['temp']),
-                      _infoBox("Water", plantDetails!['water']),
-                    ],
-                  ),
-                ),
+          child: Column(
+            children: <Widget>[
+              imageDisplayCreation(Image.memory(imageData),croppedHeight),
+              const Padding(
+                padding: EdgeInsets.all(40.0),
+                child: Text("Unable to identify the plant. Please retake the picture.", style: TextStyle(fontSize: 20) ,textAlign: TextAlign.center,),
               ),
             ],
-          )
-        // : Center(
-        //     child: Text(
-        //       "Unclear picture, please take another one!",
-        //       style: TextStyle(fontSize: 16),
-        //       textAlign: TextAlign.center,
-        //     ),
-        //   ),
-  ),
-);
+          ),
+        )) : 
+        
+        Container(
+          height: screenSize.height,
+        decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Color.fromARGB(117, 44, 120, 101), Color.fromARGB(223, 217, 237, 191)],
+                ),
+              ),
+          child: Column(
+                  
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                     
+                        
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 13.0),
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 8.0),
+                              child: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                    
+                                children: [
+                                  Text(
+                                    "Name: ${plantDetails!['name']}",
+                                    style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),textAlign: TextAlign.left,
+                                  ),
+                                  Text(
+                                    "Scientific Name: ${plantDetails!['scientific_name']}",
+                                    style: TextStyle(fontSize: 10, fontStyle: FontStyle.italic),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        
+                      
+                    
+                    imageDisplayCreation(Image.memory(imageData),croppedHeight),
+                    Padding(padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            _infoBox("Sunlight", plantDetails!['sunlight'], Colors.deepOrange, Color.fromARGB(255, 255, 153, 0)),
+                            _infoBox("Temperature", plantDetails!['temp'], Colors.green, Color.fromARGB(255, 144, 210, 109)),
+                            _infoBox("Water", plantDetails!['water'], Colors.blue, Colors.blueAccent),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(left:16.0,right:16.0), child : Container(
+                      decoration: BoxDecoration(                  color: const Color.fromARGB(255,44, 120, 101),
+        borderRadius: BorderRadius.circular(8)),
+                      child: 
+                        Padding(
+                          
+                          padding: const EdgeInsets.all(16.0),
+                          child: Text(plantDetails!['about'], style: TextStyle(color: Color.fromARGB(202, 255, 255, 255)),),
+                        ),
+                      
+                    )),
+                   
+                  ],
+                ),
+        )
+          );
+      }
+    );
 
   }
 
-  Widget _infoBox(String title, dynamic value) {
+  Widget _infoBox(String title, dynamic value, Color bordercolor, Color backgroundColor) {
     return Container(
       padding: EdgeInsets.all(8),
       decoration: BoxDecoration(
-        border: Border.all(color: Colors.green),
+        color: backgroundColor,
+        border: Border.all(color: bordercolor),
         borderRadius: BorderRadius.circular(8),
       ),
       child: Column(
--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(1, 217, 237, 191)),
         useMaterial3: true,
+        fontFamily: "Jost",
       ),
       home: const MyHomePage(title: 'Green Health'),
     );
@@ -34,19 +35,17 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'Snap a picture of your plant to get started!',
-            ),
+            const Text("Welcome", style: TextStyle(fontFamily: "Jost",fontSize: 36.0, fontWeight:FontWeight.bold)),
+            const Padding( padding: EdgeInsets.only(top:81.0),child: Text(
+              'Snap a pic of your plant to get started!',textAlign: TextAlign.center,
+             style: TextStyle(fontFamily: "Jost",fontSize: 20.0))),
             Padding(
-              padding: EdgeInsets.only(top: 24.0),
+              padding: const EdgeInsets.only(top: 140.0),
               child: ElevatedButton(
               onPressed: () {
                 Navigator.push(
@@ -55,6 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 );
               },
               style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(222, 144, 210, 109),
                 shape: const CircleBorder(),
                 padding: const EdgeInsets.all(24.0)
               ),
